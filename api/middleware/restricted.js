08134e2fba543094a5371c4e47fc928a4e787fb4
Peter Conley <peterdavidconley@gmail.com>
2022-02-25 09:39:05 -0500

    wrote restricted middleWare

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken');
+JWT_SECRET: process.env.JWT_SECRET || 'shh'
+
 module.exports = (req, res, next) => {
   
   /*
@@ -14,10 +17,14 @@ module.exports = (req, res, next) => {
 
       const token = req.headers.authorization
       if (!token) {
-        res.status(401).json({message: 'token required' })
-        next()
-      } else {
-
-      }
+        return next({ status: 401, message: 'token required'})
+      } jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+        if (err) {
+            next({ status: 401, message: 'token invalid' })
+        } else {
+          req.decodedToken = decodedToken
+          next()
+        }
+    })
 
 };
